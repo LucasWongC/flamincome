e6a311ea26d2bde7721d32a8f279e7464c23252f
LucasWongC <LucasWongC@users.noreply.github.com>
2022-11-30 16:23:49 +0000

    deploy: f3d47dc74a9ba9e7e9b7a67532d46a6222ffbd67

---
+++ precache-manifest.767b99e9d891eee405d50950e4987b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca6bb4f09bc15f7d1a2a6cf4d7a3406",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30721a00aab285169744",
+    "url": "/static/css/main.affa3bc2.chunk.css"
+  },
+  {
+    "revision": "4a937a8af97060409068",
+    "url": "/static/js/2.d59985f8.chunk.js"
+  },
+  {
+    "revision": "01e78a743a56753e6f8d36e4b90a9363",
+    "url": "/static/js/2.d59985f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30721a00aab285169744",
+    "url": "/static/js/main.ba0044ea.chunk.js"
+  },
+  {
+    "revision": "d12b4235d01972c06f10",
+    "url": "/static/js/runtime-main.d565b2e4.js"
+  }
+]);
